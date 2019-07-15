b85f64066fd7f154ed637b27c6cde2004dd33710
viergo <cemper86@gmail.com>
2019-07-15 15:29:25 +0500

    //upd:

--- app/src/main/java/ru/stairenx/konturtasktest/MainActivity.java
+++ app/src/main/java/ru/stairenx/konturtasktest/MainActivity.java
@@ -1,8 +1,14 @@
 package ru.stairenx.konturtasktest;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -17,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loadFragment(MainFragment.getInstance());
+        checkPermissionOnThisApplication();
 
     }
 
@@ -25,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         fragmentTransaction.replace(R.id.fragment, fragment).commit();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void checkPermissionOnThisApplication() {
+        int permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        if (permissionStatus != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET, Manifest.permission.CAMERA}, 1);
+        }
     }
 
 }
